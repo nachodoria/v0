de91ca71ebe21329ff7506c12dbd7f0230b7df96
Ignacio Doria <ignaciodoriaoberman@gmail.com>
2022-06-29 10:54:59 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import "./app.css"
+import { Link, animateScroll as scroll } from "react-scroll";
+
 
 function App() {
   return (
     <>
+    <div className='container-top-button'>
+      <Link to = "container" 
+         smooth = {true} 
+         offset={-70}
+         duration={800}
+          id='top-button' class="material-symbols-outlined">
+     keyboard_double_arrow_up
+     </Link>
+    
+    </div>
+    
+    <div className='container'>
+     
+     <div className='grid-main'>
+       <div className='grid-left'>
+         <h1>Hi ! <br></br> Im Ignacio Doria</h1>
+         <Link to = "ab-me"
+          smooth= {true}
+          offset={-70}
+         duration={500}
+         >
+           About Me </Link>
+       </div>
+     
+       <div className='grid-right'>
+         <img className='img-me' src={require ('./components/me.jpeg')}/>
+       </div>
+      
+     </div>
+     
+   </div>
+   
+      <div className='ab-me'>
+       <h1>About Me</h1>
+        </div>
+        
+    
+    
     </>
+     
+    
+  
+  
   );
 }
 
